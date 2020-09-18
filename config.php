514e6d1ca9bec20fd8bdcba84f7e21ebba6b8cfd
Egidijus <egidijus@es4b.lt>
2020-09-18 10:46:34 +0300

    Initial commit

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://kristallashes.com/');
+// HTTPS
+define('HTTPS_SERVER', 'http://kristallashes.com/');
+// DIR
+define('DIR_APPLICATION', '/home/u367386812/domains/kristallashes.com/public_html/catalog/');
+define('DIR_SYSTEM', '/home/u367386812/domains/kristallashes.com/public_html/system/');
+define('DIR_IMAGE', '/home/u367386812/domains/kristallashes.com/public_html/image/');
+define('DIR_STORAGE', DIR_SYSTEM . 'storageu95qf22shqt6/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/theme/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', '127.0.0.1');
+define('DB_USERNAME', 'u367386812_TwcbI');
+define('DB_PASSWORD', 'RmbST55qKI');
+define('DB_DATABASE', 'u367386812_lGuW1');
+define('DB_PREFIX', 'avks_');
+define('DB_PORT', '3306');
---
+++ system/storageu95qf22shqt6/cache/template/1c/1cc95d36063b6e7ec6b735123d627fd36929f569699e66a3355dbfaa6c7c76d7.php
@@ -0,0 +1,75 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* journal3/template/common/maintenance.twig */
+class __TwigTemplate_c1e23ad09a2dcfd055362a47bd28b3dfeba376350f1fba691767c8c42c28c926 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 1
+        echo ($context["header"] ?? null);
+        echo "
+<div id=\"common-maintenance\" class=\"container\">
+    ";
+        // line 3
+        echo twig_get_attribute($this->env, $this->source, twig_get_attribute($this->env, $this->source, ($context["j3"] ?? null), "settings", [], "any", false, false, false, 3), "get", [0 => "maintenanceContent"], "method", false, false, false, 3);
+        echo "
+    ";
+        // line 4
+        echo ($context["grid"] ?? null);
+        echo "
+</div>
+";
+        // line 6
+        echo ($context["footer"] ?? null);
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "journal3/template/common/maintenance.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  51 => 6,  46 => 4,  42 => 3,  37 => 1,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "journal3/template/common/maintenance.twig", "");
+    }
+}
